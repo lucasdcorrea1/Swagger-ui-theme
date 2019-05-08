9d539617be3d3354cb1cd9d2b2559b219d77f406
Lucas Damas <suporte@cloudmed.io>
2019-05-07 23:48:09 -0300

    Padão Swagger

--- README.md
+++ README.md
@@ -2,3 +2,41 @@
 # API Rest .NET CORE 
 easy implementation template for swagger ui
 
+# Rota
+├── API
+    ├── wwwroot
+        ├──docs
+            ├──css
+            ├──index        
+            ├──js
+            ├──img
+            ├──favicon-16x16.png
+            ├──favicon-32x32.png
+            
+        
+
+Startp.cs
+
+    code 
+
+        app.UseAuthentication();
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseCors("SiteCorsPolicy");
+
+
+
+        app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "company Authorization API");
+                c.RoutePrefix = "docs";
+                c.DocumentTitle = "Name API";
+                c.InjectStylesheet("css/swagger.css");
+                c.InjectStylesheet("css/theme-material.css");
+                c.InjectStylesheet("css/style.css");
+                c.InjectStylesheet("https://use.fontawesome.com/releases/v5.7.0/css/all.css");
+                c.InjectStylesheet("https://fonts.googleapis.com/css?family=Maven+Pro");
+                c.InjectJavascript("https://code.jquery.com/jquery-3.3.1.min.js");
+                c.InjectJavascript("js/on-complete.js");
+
+            });
---
+++ wwwroot/docs/js/on-complete.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    loadpage();
+});
+
+function loadpage() {
+    setTimeout(function () {
+        var div = $(".wrapper > .col-12-desktop");
+        $("body").show().html("<div class='cssload-box-loading'>" +
+            "</div>");
+
+        setTimeout(function () {
+            organizeComponents(div);
+        }, 2300);
+    }, 1500);
+};
+
+function checkedtrue() {
+    $("#cb-1").prop("checked", true);
+    $("#cb-2").prop("checked", true);
+    $("#cb-3").prop("checked", true);
+};
+
+function openNav() {
+    document.getElementById("mySidenav").style = "258px";
+    document.getElementById("main2").style.marginLeft = "258px";
+    document.getElementById("main3").style.marginLeft = "258px";
+};
+
+function closeNav() {
+    document.getElementById("mySidenav").style = "display:none";
+    document.getElementById("main2").style.marginLeft = "0";
+    document.getElementById("main3").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+};
+
+
+function organizeComponents(div) {
+    $.get("index/index.html", function (data) {
+        $("body").html(data);
+        $(document).ready(function () {
+            openNav()
+            $("#dataLocal").html(div[0]);
+
+        });
+    });
+}
